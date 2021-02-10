b0d222869c801fc768929aaf3da460292c4260d3
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-02-10 10:52:47 +0100

    corrections

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -5,14 +5,18 @@ export default function Pagination({postPerPage,totalPosts, paginate}) {
     for(let i = 1; i<= Math.ceil(totalPosts/postPerPage);i++){
         pageNumbers.push(i)
     }
+    
+    
     return (
         
        <nav className="flex justify-center">
            <ul className="flex list-reset  w-auto">
                {pageNumbers.map(number => (
-                   <li key={number}>
+                   <li key={number}
+                   onClick={() => window.scrollTo(0,0)}>
                        <a 
                        onClick={() => paginate(number)}
+                       
                        className="block hover:text-white hover:bg-blue-500 hover:border-blue-500 text-blue border border-grey-light px-3 py-2">
                        {number}</a>
                    </li>
