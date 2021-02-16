5d2f3de5b6f753bf5ef2a31af32e8d425badf39d
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-02-16 21:58:23 +0100

    contact us

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faYoutubeSquare,faPinterestSquare,faInstagram,faFacebookSquare,faTwitterSquare,faCoffee} from '@fortawesome/free-solid-svg-icons';
 
 export default function Footer() {
     return (
         <footer className={'bg-gray-200 text-center text-xs p-3 absolute bottom-0 w-full'}>
             &copy; Copyright 2021
+            <FontAwesomeIcon
+        icon={faCoffee}
+        />
         </footer>
     )
 }
