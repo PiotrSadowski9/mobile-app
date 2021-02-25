6987998118c254b443ee9faa47ef7e5b64175c3e
Piotr Sadowski <piotrsadowski008@gmail.com>
2021-02-25 22:56:23 +0100

    cart fixes

--- src/Views/Cart.js
+++ src/Views/Cart.js
@@ -16,31 +16,44 @@ export default function Cart({user, setUser}) {
 
     
     
-    let products = user.map(e => {
-        return <li key={e.name}>
-                    Nazwa: {e.name} 
-
-                    Cena: {e.price} 
-                    <FontAwesomeIcon 
-                            icon={faMinus}
-                            onClick={() =>deleteItem(e.name)}
-                            
-                          />
+    let products = user.map((e,index) => {
+         if(index % 2 == 0) {
+             return <tr>
+                        <td className="w-1/3 text-left py-3 px-4">{e.name}</td>
+                        <td className="w-1/3 text-left py-3 px-4">{e.price}</td>
+                        <td className="text-left py-3 px-4"><FontAwesomeIcon icon={faMinus} onClick={() =>deleteItem(e.name)}/></td>
+                    </tr>
+               }  
+        return  <tr class="bg-gray-100">
+                    <td class="w-1/3 text-left py-3 px-4">{e.name}</td>
+                    <td class="w-1/3 text-left py-3 px-4">{e.price}</td>
+                    <td class="text-left py-3 px-4"><FontAwesomeIcon icon={faMinus} onClick={() =>deleteItem(e.name)}/></td>
                     
-               </li>
-
+                </tr>         
     })
 
 
 
-    return (
-        <div>
-            <h1>Twój koszyk</h1>
-            <ul>
-                {products}
-            </ul>
-
-            <p>Suma:{sum}</p>
-        </div>
+    return (<>
+            <div className="md:px-32 py-8 w-full">
+                <div className="shadow overflow-hidden rounded border-b border-gray-200">
+                    <table className="min-w-full bg-white">
+                    <thead className="bg-gray-800 text-white">
+                        <tr>
+                        <th className="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Name</th>
+                        <th className="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Price</th>
+                        <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Delete</th>
+                        
+                        </tr>
+                    </thead>
+                    <tbody className="text-gray-700">
+                    {products}
+                    
+                    </tbody>
+                    </table>
+                </div>
+            </div>
+            <h1>{sum}</h1>
+</>
     )
 }
